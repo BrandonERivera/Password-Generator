4dab11dcd0c4da2e12b9580147d74e604545b051
BrandonERivera <brandonellis.rivera@gmail.com>
2023-08-09 23:14:56 -0400

    added comments to script.js

--- script.js
+++ script.js
@@ -3,20 +3,24 @@ var generateBtn = document.querySelector("#generate");
 
 function generatePassword()
 {
-  // getting variables
+  //Variables
+
+  // Creating variables the prompts will ask the question for length.
   var passwordsize = window.prompt("enter length of password");
+  // It will grab the length of the password in passarr, and create an empty array for it to fill.
   var passarr = [];
   passarr.length = passwordsize;
-
+  // chararray is for later when asking about the characters to include and not include
   var chararray = [0,0,0,0];
-
+  // arrays alphabets and spchar are characters the computer will choose from for the password.
   var alphabets = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
   var spchar = ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=",":",";","[","]","{","}","'","|",",",".","<",">","?","/"];
-
+  // var character will be returned and will updated to fill in the passarr.
   var character;
 
-// Other functions
+//Functions
+//This function is how it will choose randomly for character types.
 function ChooseACharacter()
 {
   var selectedFunction = Math.floor(Math.random() * 4);
@@ -37,7 +41,9 @@ function ChooseACharacter()
   {
     ChooseASpChar()
   }
-
+//Functions ChooseA------ is for choosing a random letter/number/symbol in its array (or in nums case 0-9)
+//all have else to ChooseACharacter because it checks if chararray has a Y/N in it and if it should include
+// or not include said character type.
 function ChooseALowerLetter()
 {
   if(chararray[0] === "Y")
@@ -94,17 +100,15 @@ function ChooseASpChar()
 }
 
 }
-
-
-
-
-  //answering the question prompts
+  //These are for answering the questions, it also makes sure the password is an apporpriate size and if not, or if no character
+  //types are acceptable then it will not proceed.
   if(passwordsize < 8 || passwordsize > 128 )
   {
     window.alert("not a valid number")
   }
   else
   {
+    //heres where chararray is changed and ChooseACharacter function will know what characters to include or not.
     var YNletters = window.confirm("Click OK if you would like lowercase letters. If not click Cancel");
     if(YNletters)
     {
@@ -150,7 +154,8 @@ function ChooseASpChar()
     }
     else
     {
-      //This is where function will be called to replace the letter
+      //This is where function will be called to replace each value in array passarr with character that
+      // is returned from choose a letter.
       for(i = 0; i < passarr.length; i++)
       {
         ChooseACharacter()
@@ -158,6 +163,7 @@ function ChooseASpChar()
       }
     }
   }
+  //it is all then joined into one string without comma's
   return passarr.join("");
 }
 
